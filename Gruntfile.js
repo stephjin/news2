fd8f9e6f66b5acdb0bfd392d33ff32d6385a78fd
stephjin <stephanie.k.jin@gmail.com>
2014-12-26 00:01:26 -0500

    Add Grunt.

---
+++ Gruntfile.js
@@ -0,0 +1,13 @@
+module.exports = function(grunt){
+  grunt.initConfig({
+    nodemon: {
+      dev: {
+        script: 'server.js'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-nodemon');
+
+  grunt.registerTask('default', ['nodemon']);
+}
