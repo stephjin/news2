daf738acf93ca0e21fa715c1bc9fd75f1ab2ab00
stephjin <stephanie.k.jin@gmail.com>
2014-12-26 00:10:38 -0500

    Add mongoose and body parser modules.

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 var express = require('express'),
     app = express(),
-    port = process.env.PORT || 3000;
+    port = process.env.PORT || 3000,
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost/news2');
 
 app.listen(port);
 console.log('Server is running on port: ' + port);
